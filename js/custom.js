2e3643d8fc90e3087c4277e810b746f74902aff6
Nick Barth <barth.nicholas@gmail.com>
2015-12-13 22:00:38 -0500

    initial

---
+++ js/custom.js
@@ -0,0 +1,26 @@
+function GetCurrentPageName() { 
+//method to get Current page name from url. 
+var PageURL = document.location.href; 
+var PageName = PageURL.substring(PageURL.lastIndexOf('/') + 1); 
+ 
+return PageName.toLowerCase() ;
+}
+ 
+$(document).ready(function(){
+var CurrPage = GetCurrentPageName();
+ 
+switch(CurrPage){
+case 'index.php':
+ $('#li_home').addClass('active') ;
+ break;
+case 'about.php':
+ $('#li_about').addClass('active') ;
+ break;
+case 'contact.php':
+ $('#li_area').addClass('active') ;
+ break;
+ case 'contact.php':
+ $('#li_attorn').addClass('active') ;
+ break;
+}
+});
